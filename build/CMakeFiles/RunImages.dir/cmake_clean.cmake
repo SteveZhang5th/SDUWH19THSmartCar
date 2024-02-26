faf980517d6d18bd8233b341b4a57233c46bebcf
userName <email address>
2024-02-26 20:52:23 +0800

    git初次使用版本

---
+++ build/CMakeFiles/RunImages.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RunImages.dir/src/cap.c.o"
+  "CMakeFiles/RunImages.dir/src/comm.c.o"
+  "CMakeFiles/RunImages.dir/src/serial.cpp.o"
+  "CMakeFiles/RunImages.dir/src/MyUart.cpp.o"
+  "CMakeFiles/RunImages.dir/src/trvision.cpp.o"
+  "CMakeFiles/RunImages.dir/src/MyMath.cpp.o"
+  "CMakeFiles/RunImages.dir/RunImages.cpp.o"
+  "RunImages.pdb"
+  "RunImages"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/RunImages.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/pthreadcap.dir/cmake_clean.cmake
@@ -0,0 +1,16 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pthreadcap.dir/src/cap.c.o"
+  "CMakeFiles/pthreadcap.dir/src/comm.c.o"
+  "CMakeFiles/pthreadcap.dir/src/serial.cpp.o"
+  "CMakeFiles/pthreadcap.dir/src/MyUart.cpp.o"
+  "CMakeFiles/pthreadcap.dir/src/trvision.cpp.o"
+  "CMakeFiles/pthreadcap.dir/src/MyMath.cpp.o"
+  "CMakeFiles/pthreadcap.dir/pthreadcap.cpp.o"
+  "pthreadcap.pdb"
+  "pthreadcap"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/pthreadcap.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
